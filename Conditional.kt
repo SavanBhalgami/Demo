b35c3eb7bbc0e2033b516b8e6db1c8389191c62f
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:16:46 +0530

    Add files via upload

---
+++ Conditional.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+	val num1=40
+	val num2=50
+	
+	if(num1>num2)
+	{
+		println("Num1 is Maximum")
+	}
+	else if(num1==num2)
+	{
+		println("Num1 & Num2 is Equal")
+	}
+	else
+	{
+		println("Num2 is Maximum")
+	}
+}
