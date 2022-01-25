c33015ddb1f25cc667f2ae9f4f6cf8d80966dcf5
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:05:29 +0530

    Add files via upload

---
+++ Variable.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+	println("Immutable Variable")
+	val valvb:String="Hello"
+	println("$valvb")
+
+	println()
+	println("Mutable Varibale")
+	var varvb:String="ABC"
+	varvb="DDD"
+	println("$varvb")	
+}
