1132c94aeb3b480c7580508ae44fe1921e2738a8
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:44:06 +0530

    Add files via upload

---
+++ DataClass.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+	data class student(val enno:Int,val name:String)
+	
+	val std1=student(1,"Demo")
+	val std2=student(2,"Second Demo")
+	
+	println(std1.toString())
+	println(std2.toString())
+	
+}
