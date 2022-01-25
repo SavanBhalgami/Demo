b9afd00b60687a5b69f933da1998baecfb8d19db
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:20:48 +0530

    Add files via upload

---
+++ Loop.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+	println("For Loop Demo..")
+	val a=arrayOf("A","B","C","D")
+	for(item in a)
+	{
+		println(item)
+	}
+	
+	println("While Loop Demo..")
+	var i=1
+	while(i<=4)
+	{
+		println("Number $i")
+		i++
+	}
+}
