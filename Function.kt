4a0d4c1f155154acaff8fe8d663680f78ce4e290
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:12:51 +0530

    Add files via upload

---
+++ Function.kt
@@ -0,0 +1,17 @@
+fun add(n1:Int,n2:Int)
+{
+	var n3=n1+n2
+	println("Addition is :- $n3")
+}
+
+fun mul(n1:Int,n2:Int)
+{
+	var n3=n1*n2
+	println("Multiplication is :- $n3")
+}
+
+fun main()
+{
+	add(5,5)
+	mul(5,5)	
+}
