60a825a700441e439ac1482c18572229d6f6c9e8
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:30:35 +0530

    Add files via upload

---
+++ CODemo.kt
@@ -0,0 +1,17 @@
+class coDemo
+{
+	val name:String="Demo"
+	val age:Int=21
+	
+	fun display()
+	{
+		println("Name : $name")
+		println("Age : $age")
+	}
+}
+
+fun main()
+{
+	val ob=coDemo()
+	ob.display()
+}
