960f50eb95ac19bb4e32c7a10554ec7649b1cb30
SavanBhalgami <97604026+SavanBhalgami@users.noreply.github.com>
2022-01-25 14:57:25 +0530

    Update Enum.kt

--- Enum.kt
+++ Enum.kt
@@ -8,10 +8,10 @@ fun main()
 {
 	for(day in DAY.values())
 	{
-		println("${day.ordinal} = ${day.name}")  //ordinal: start with zero index  //name: store the name of the constant
+		println("${day.ordinal} = ${day.name}")  
 	}
 	println()
 	
-	println("${DAY.valueOf("Sunday")}")   //valueOf: returns the define enum otherwise IllegalArgumentException
+	println("${DAY.valueOf("Sunday")}")   
 
 }
